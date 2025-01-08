d8893d07d19b1b9963cccfefa84543d96ed778ba
john.markson <john.markson@itm-development.com>
2025-01-09 02:04:15 +0300

    auth

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,34 +1,82 @@
-'use client'
+"use client";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function SignupPage() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [creatingUser, setCreateingUser] = useState(false);
+  const [createdUser, setCreatedUser] = useState(false);
+  const [error, setError] = useState(false);
 
-    function handleFormSubmit(e) {
-        e.preventDefault()
-        fetch('/api/signup',{
-            method: 'POST',
-            body: JSON.stringify({email, password}),
-            headers: {'Content-Type': 'application/json'}
-        })
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+    setCreateingUser(true);
+    const response = await fetch("/api/signup", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      setCreatedUser(true);
+    } else {
+      setError(true);
     }
+    setCreateingUser(false);
+  }
 
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Sign up</h1>
-      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit} >
-        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value) } />
-        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value) } />
-        <button type="submit">Sign up</button>
+      {createdUser && (
+        <div className="my-4 text-center">
+          User created.
+          <br />
+          Now you can{" "}
+          <Link className="underline" href={"/login"}>
+            Login &raquo;
+          </Link>
+        </div>
+      )}
+      {error && (
+        <div className="my-4 text-center">
+          An error has occurred.
+          <br />
+          Please try again later
+        </div>
+      )}
+      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={creatingUser}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          disabled={creatingUser}
+        />
+        <button type="submit" disabled={creatingUser}>
+          Sign up
+        </button>
         <div className="my-4 text-center text-gray-500">
           or login with provider
         </div>
-        <button className="flex gap-4 justify-center" >
+        <button className="flex gap-4 justify-center">
           <Image src={"/google.png"} alt={""} width={24} height={24} />
           Login with google
         </button>
+        <div className="text-center my-4 text-gray-500 border-t pt-4">
+          Existing account?{" "}
+          <Link className="underline" href={"/login"}>
+            Login here &raquo;
+          </Link>
+        </div>
       </form>
     </section>
   );
