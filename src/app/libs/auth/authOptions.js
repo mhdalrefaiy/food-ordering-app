e56546da1c1badef99b7802ba7dd3be6d73ab50f
john.markson <john.markson@itm-development.com>
2025-10-08 04:33:11 +0300

    fix errors

---
+++ src/app/libs/auth/authOptions.js
@@ -0,0 +1,43 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "../mongoConnect";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import * as mongoose from "mongoose";
+import { User } from "@/app/models/User";
+import bcrypt from "bcrypt";
+
+export const authOptions = {
+  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      id: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({ email });
+        const passwordOk = user && bcrypt.compareSync(password, user.password);
+
+        if (passwordOk) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
+};
