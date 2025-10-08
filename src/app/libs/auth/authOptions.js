b3f365a643d659782e1c35b51a13b3a9abd12ee9
john.markson <john.markson@itm-development.com>
2025-10-08 05:55:25 +0300

    fix2

--- src/app/libs/auth/authOptions.js
+++ src/app/libs/auth/authOptions.js
@@ -7,7 +7,7 @@ import { User } from "@/app/models/User";
 import bcrypt from "bcrypt";
 
 export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.SECRET,
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
